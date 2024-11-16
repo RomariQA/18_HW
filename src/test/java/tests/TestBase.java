1b8f66585269349927d39297b9df022bacdf822b
Maya Romanovich <mariaromanovich@yandex.ru>
2024-11-17 00:45:59 +0300

    added fixes

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,12 +1,17 @@
 package tests;
 
+import com.codeborne.selenide.logevents.SelenideLogger;
 import helpers.Attach;
 
+import io.qameta.allure.selenide.AllureSelenide;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import com.codeborne.selenide.Configuration;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
+
+import java.util.Map;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 
@@ -24,6 +29,14 @@ public class TestBase {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
 
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
+
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
 
     @AfterEach
